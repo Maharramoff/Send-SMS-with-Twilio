f16dc73545cba72becb7591f9a55e0e431f4208c
Maharramoff <sh.maharramov@gmail.com>
2020-04-15 02:42:03 +0400

    handle env variables

--- app/Main.hs
+++ app/Main.hs
@@ -4,19 +4,28 @@
 module Main where
 
 import Network.HTTP.Client
-import Network.HTTP.Client.TLS (getGlobalManager, tlsManagerSettings)
+import Network.HTTP.Client.TLS (tlsManagerSettings)
 import Network.HTTP.Types.Status (statusCode)
-import Data.Aeson (object, (.=), encode)
+import LoadEnv
+import System.Environment
+import qualified Data.ByteString as B
+import Data.ByteString.UTF8 as BU
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
+
+getEnvBS :: String -> IO B.ByteString
+getEnvBS = fmap (T.encodeUtf8 . T.pack) . getEnv
 
 main :: IO ()
 main = do
+  loadEnv
+  sid <- getEnvBS "TWILIO_ACCOUNT_SID"
+  token <- getEnvBS "TWILIO_AUTH_TOKEN"
   manager <- newManager tlsManagerSettings
   let body = [("Body", "Hello World!"), ("From", "+12052728785"), ("To", "+994518010000")]
-  let url  = "https://api.twilio.com/2010-04-01/Accounts/ACa38ae9fef48d112839874f279fddcb94/Messages.json"
+  let url = "https://api.twilio.com/2010-04-01/Accounts/" ++ BU.toString sid ++ "/Messages.json"
   initialRequest <- parseUrlThrow url
-  let request = applyBasicAuth "ACa38ae9fef48d112839874f279fddcb94" "6565eb69a6a8922d4689caae6436bb4b" $
-                urlEncodedBody body $
-                initialRequest {method = "POST"}
+  let request = applyBasicAuth sid token $ urlEncodedBody body $ initialRequest {method = "POST"}
   response <- httpLbs request manager
   putStrLn $ "Response status: " ++ show (statusCode $ responseStatus response)
   --print $ responseBody response
